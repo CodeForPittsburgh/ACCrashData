2997e5bb1286ebd1b7f90d45a40af71fa4f1ece2
mhowe0422 <mhowe0422@msn.com>
2016-07-02 07:32:57 -0400

    Scripts to create PostgreSQL Month of year table
    Yes, you'll need to add the months, January = 1

---
+++ month_of_year.sql
@@ -0,0 +1,15 @@
+﻿-- Table: public.month_of_year
+
+-- DROP TABLE public.month_of_year;
+
+CREATE TABLE public.month_of_year
+(
+  id integer NOT NULL,
+  name character varying(9),
+  CONSTRAINT month_of_yearid PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.month_of_year
+  OWNER TO postgres;
